3d6fc4527ce399214bff84e7b889133027c8e909
Dania592 <118030608+Dania592@users.noreply.github.com>
2023-11-22 23:04:25 +0100

    Root to new pages (GET and POST method) linked with Controller

--- public/index.php
+++ public/index.php
@@ -1,21 +1,27 @@
 <?php
 declare(strict_types=1);
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Factory\AppFactory;
 
+use App\Controller\EventsController;
+use App\Controller\RegisterController;
+use Slim\Factory\AppFactory;
 use App\Controller\HomeController;
 use App\Controller\AccountController;
-use App\Controller\RegisterController;
+use App\Controller\ConnexionController;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, false, false);
+$app->addBodyParsingMiddleware();
 
-$app->get('/', HomeController::class.':index')->setName('home');
-$app->get('/account', AccountController::class.':index')->setName('account');
-$app->get('/register', RegisterController::class.':index')->setName('register');
+$app->get('/', HomeController::class.':index');
+$app->get('/account', AccountController::class.':index');
+$app->post('/account', AccountController::class.':index');
+$app->get('/register', RegisterController::class.':index');
+$app->post('/register', RegisterController::class.':post');
+$app->get('/events', EventsController::class.':index');
+$app->get('/connexion', ConnexionController::class.':index');
+$app->post('/connexion', ConnexionController::class.':post');
 
 $app->run();
