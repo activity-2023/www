c1c49983010441f41d3c09ce9ab765bfcfab1678
Dania592 <118030608+Dania592@users.noreply.github.com>
2023-11-26 09:43:30 +0100

    Adding roots to new pages

--- public/index.php
+++ public/index.php
@@ -1,27 +1,50 @@
 <?php
 declare(strict_types=1);
 
+use App\Controller\AccountController;
+use App\Controller\ActivitiesController;
+use App\Controller\ActivityController;
 use App\Controller\EventsController;
 use App\Controller\RegisterController;
+use App\Controller\SubscribeController;
 use Slim\Factory\AppFactory;
 use App\Controller\HomeController;
-use App\Controller\AccountController;
+use App\Controller\RoomChoiceController;
 use App\Controller\ConnexionController;
 
 require __DIR__.'/../vendor/autoload.php';
+
+session_start();
 
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, false, false);
 $app->addBodyParsingMiddleware();
 
+
 $app->get('/', HomeController::class.':index');
 $app->get('/account', AccountController::class.':index');
 $app->post('/account', AccountController::class.':index');
+
 $app->get('/register', RegisterController::class.':index');
 $app->post('/register', RegisterController::class.':post');
-$app->get('/events', EventsController::class.':index');
+
+$app->get('/subscribe/{id}', SubscribeController::class.':index');
+
 $app->get('/connexion', ConnexionController::class.':index');
 $app->post('/connexion', ConnexionController::class.':post');
+$app->get('/deconnexion', ConnexionController::class.':deconnexion');
+
+$app->get('/activities', ActivitiesController::class.':index');
+$app->get('/activity/{id}', ActivityController::class.':index');
+
+$app->post('/activitycreation',ActivitiesController::class.':activityAdd');
+$app->get('/activitycreation',ActivitiesController::class.':createActivity');
+
+$app->get('/event/{id}',EventsController::class.':index');
+$app->post('/event',EventsController::class.':post');
+$app->get('/inscription/event={id_event}', EventsController::class.':inscription');
+$app->get('/reserve/id_room={room_id}/event_date={eventDate}/event_start={eventStartTime}/event_duration={eventduration}/event_max_participant={eventMaxParticipant}/id_activity={activityId}',RoomChoiceController::class.':index');
+
 
 $app->run();
