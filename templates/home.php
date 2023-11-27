944ec9a5427822d422e461c203abd701b587c353
Dania592 <118030608+Dania592@users.noreply.github.com>
2023-11-27 22:10:44 +0100

    Adding content to home page

--- templates/home.php
+++ templates/home.php
@@ -1,5 +1,35 @@
-<h1>Activity</h1>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores blanditiis consequatur
-    corporis eos libero, non nostrum nulla. Alias delectus earum eius explicabo facere maiores nam
-    obcaecati quis temporibus veniam!</p>
+<section>
+    <h1>Ne rater aucun événement !</h1>
+    <article>
+        <h5>Événements en mouvement, gestion sans tourment, notre plateforme, votre atout abondant!</h5>
+        <div style="align-items: center; text-align: center; padding: 2em">
+            <div class="card" style="width: 80%;">
+                <div class="card-body">
+                    <p>Créez, partagez, vivez ! Simplifiez la gestion de vos événements et activités avec notre
+                        plateforme intuitive. De l'idée à la réalité, faites de chaque moment une expérience
+                        inoubliable. Prêt à organiser sans tracas ? Bienvenue ! </p>
+                </div>
+            </div>
+        </div>
+        <div class="images_container">
+            <figure>
+                <img src="sources/atelier.jpg" alt="Atelier"  class="images">
+                <figcaption>Participez à nos Atelier</figcaption>
+            </figure>
+            <figure>
+                <img src="sources/cours.png" alt="Cours"  class="images">
+                <figcaption>Rattrapez vos lacunes</figcaption>
+            </figure>
+            <figure>
+                <img src="sources/reunion.jpg" alt="Réunion"  class="images">
+                <figcaption>Organisez et assistez à vos réunion</figcaption>
+            </figure>
+
+        </div>
+
+    </article>
+</section>
+
+
+
