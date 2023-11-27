362aa83e25edb13b840540fcf07154e2844caefa
Dania592 <118030608+Dania592@users.noreply.github.com>
2023-11-27 22:26:19 +0100

    Adding final roots to pages

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use App\Controller\AccountController;
 use App\Controller\ActivitiesController;
 use App\Controller\ActivityController;
 use App\Controller\CancelController;
+use App\Controller\ChildController;
 use App\Controller\ConnexionController;
 use App\Controller\EventsController;
 use App\Controller\RegisterController;
@@ -44,7 +45,9 @@ $app->get('/activitycreation',ActivitiesController::class.':createActivity');
 
 $app->get('/event/id={id}/name={name}',EventsController::class.':index');
 $app->post('/event',EventsController::class.':post');
-$app->get('/inscription/id={id_event}', EventsController::class.':inscription');
+$app->get('/inscription/id={id_event}', EventsController::class.':personChoice');
+$app->get('/inscription/id={id_event}/{person_id}', EventsController::class.':inscription');
+
 $app->get('/reserve/id_room={room_id}/event_date={eventDate}/event_start={eventStartTime}/event_duration={eventduration}/event_max_participant={eventMaxParticipant}/id_activity={activityId}',RoomChoiceController::class.':index');
 
 $app->get('/user_account_creation', StaffFonctionController::class.':index');
@@ -52,8 +55,15 @@ $app->get('/staff_account_creation', StaffFonctionController::class.':staff');
 
 $app->get('/cancel_event/id={id}', CancelController::class.':index');
 $app->get('/cancel_activity/id={id}', CancelController::class.':cancel_activity');
-$app->get('/unsubscribe_event/id={id}', CancelController::class.':unsubscribe_event');
+$app->get('/unsubscribe_event/id={id_event}', EventsController::class.':personChoice');
+$app->get('/unsubscribe_event/id={id}/{personId}', CancelController::class.':unsubscribe_event');
 $app->get('/unsubscribe_activity/id={id}', CancelController::class.':unsubscribe_activity');
 $app->get('/join_activity/id={id}',StaffFonctionController::class.':joinActivity');
 $app->get('/join_event/id={id}',StaffFonctionController::class.':joinEvent');
+
+
+$app->get('/addchild/id={id}', ChildController::class.':index');
+$app->get('/removechild/id={id}', ChildController::class.':remove');
+$app->post('/addchild', ChildController::class.':post');
+
 $app->run();
